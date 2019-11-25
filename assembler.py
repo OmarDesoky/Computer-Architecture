ff2f9b782e4ed1294c07fac88fc66499d227fd26
3amrA7med <amr.ahmed.abdelbaqi@gmail.com>
2019-11-26 01:53:42 +0200

    Added OPCODES for branch, no operand and jsr instructions in assmbler.py module

--- assembler.py
+++ assembler.py
@@ -1,56 +1,112 @@
-def setIR(str):
-    #str = "MoV r1,R2"
-    IR = 0
-    str = str.upper()
+# creating OP-CODE for the branch instructions
+class Branch:
+    def __init__(self,off):
+        self.offset = off
+        self.BR = bin(0b1010000000000000 | off)
+        self.BEQ = bin(0b1010000100000000 | off)
+        self.BNE = bin(0b1010001000000000 | off)
+        self.BLO = bin(0b1010001100000000 | off)
+        self.BLS = bin(0b1010010000000000 | off)
+        self.BHI = bin(0b1010010100000000 | off)
+        self.BHS = bin(0b1010011000000000 | off)
 
+
+class NoOperand:
+    def __init__(self):
+        self.NOP = 0b1110100000000000
+        self.HLT = 0b1110000000000000
+
+
+class Jsr:
+    def __init__(self):
+        self.JSR = 0b1111000000000000
+        self.RTS = 0b1111010000000000
+        self.INTERRUPT = 0b1111100000000000
+        self.IRET = 0b1111110000000000
+
+
+def set_ir(str, offset):
+    #str = "MoV r1,R2"
+    ir = 0
+    str = str.upper()
+    branch = Branch(offset)
+    noOperand = NoOperand()
+    jsr = Jsr()
     temp = ""
     for i in range(len(str)):
 
         if str[i] == " ":
             if temp == "MOV":
-                IR = IR | 0x00000000
+                ir = ir | 0x00000000
             elif temp == "ADD":
-                IR = IR | 0x00001000
+                ir = ir | 0x00001000
             elif temp == "ADC":
-                IR = IR | 0x00002000
+                ir = ir | 0x00002000
             elif temp == "SUB":
-                IR = IR | 0x00003000
+                ir = ir | 0x00003000
             elif temp == "SBC":
-                IR = IR | 0x00004000
+                ir = ir | 0x00004000
             elif temp == "AND":
-                IR = IR | 0x00005000
+                ir = ir | 0x00005000
             elif temp == "OR":
-                IR = IR | 0x00006000
+                ir = ir | 0x00006000
             elif temp == "XNOR":
-                IR = IR | 0x00007000
+                ir = ir | 0x00007000
             elif temp == "CMP":
-                IR = IR | 0x00008000
+                ir = ir | 0x00008000
 
             elif temp == "INC":
-                IR = IR | 0x0000C000
+                ir = ir | 0x0000C000
             elif temp == "DEC":
-                IR = IR | 0x0000C040
+                ir = ir | 0x0000C040
             elif temp == "CLR":
-                IR = IR | 0x0000C080
+                ir = ir | 0x0000C080
             elif temp == "INV":
-                IR = IR | 0x0000C0C0
+                ir = ir | 0x0000C0C0
             elif temp == "LSR":
-                IR = IR | 0x0000C100
+                ir = ir | 0x0000C100
             elif temp == "RDR":
-                IR = IR | 0x0000C140
+                ir = ir | 0x0000C140
             elif temp == "RRC":
-                IR = IR | 0x0000C180
+                ir = ir | 0x0000C180
             elif temp == "ASR":
-                IR = IR | 0x0000C1C0
+                ir = ir | 0x0000C1C0
             elif temp == "LSL":
-                IR = IR | 0x0000C200
+                ir = ir | 0x0000C200
             elif temp == "ROL":
-                IR = IR | 0x0000C240
+                ir = ir | 0x0000C240
             elif temp == "RLC":
-                IR = IR | 0x0000C280
+                ir = ir | 0x0000C280
+            # Branch instructions
+            elif temp == "BR":
+                ir = ir | branch.BR
+            elif temp == "BEQ":
+                ir = ir | branch.BEQ
+            elif temp == "BNE":
+                ir = ir | branch.BNE
+            elif temp == "BLO":
+                ir = ir | branch.BLO
+            elif temp == "BLS":
+                ir = ir | branch.BLS
+            elif temp == "BHI":
+                ir = ir | branch.BHI
+            elif temp == "BHS":
+                ir = ir | branch.BHS
+            # No operand
+            elif temp == "NOP":
+                ir = ir | noOperand.NOP
+            elif temp == "HLT":
+                ir = ir | noOperand.HLT
+            # JSR instructions
+            elif temp == "JSR":
+                ir = ir | jsr.JSR
+            elif temp == "RTS":
+                ir = ir | jsr.RTS
+            elif temp == "INTERRUPT":
+                ir = ir | jsr.INTERRUPT
+            elif temp == "IRET":
+                ir = ir | jsr.IRET
             temp = ""
         else:
             temp.join(str[i])
-
-    print(str)
-    print(IR)
+    return ir
