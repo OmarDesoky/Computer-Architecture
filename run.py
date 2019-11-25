ad2c124be0aabeeee309823b1dc260e522677753
OmarDesoky <omar.ahmed98330@gmail.com>
2019-11-25 23:10:54 +0200

    fixed read function

--- run.py
+++ run.py
@@ -5,8 +5,8 @@ def read():
 	path = os.listdir('./inputs')
 	path = os.path.join('inputs','sample.txt')
 	with open(path,"r") as f:
-		inputList = f.readlines()
-
+		inputList = f.read().splitlines()
+	print(inputList)
 
 #to test function
 read()
