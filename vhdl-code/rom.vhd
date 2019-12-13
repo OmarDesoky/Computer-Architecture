2f8a4d5602c928fe90eaa11ec10988e03f6fc037
AbdElRahman ElGamil <abdurrumanmohamed@gmail.com>
2019-12-13 15:31:10 +0200

    ROM Init added

--- vhdl-code/rom.vhd
+++ vhdl-code/rom.vhd
@@ -13,7 +13,20 @@ END ENTITY ROM;
 ARCHITECTURE MyROM OF ROM IS
 
 	TYPE ROM_type IS ARRAY(0 TO (2**addressBits) - 1) OF std_logic_vector(wordSize - 1 DOWNTO 0);
-	SIGNAL ROM : ROM_type ;
+	impure function init_rom_bin return ROM_type is
+		file text_file : text open read_mode is "CONTROL_STORE.txt";
+		variable text_line : line;	
+		variable rom_content : ROM_type;
+		variable bv : bit_vector(rom_content(0)'range);
+		begin
+		for i in 0 to (2**addressBits-1) loop
+			readline(text_file, text_line);
+			read(text_line, bv);
+			rom_content(i) := To_StdLogicVector(bv);
+		end loop;
+  		return rom_content;
+	end function;
+	SIGNAL ROM : ROM_type := init_rom_bin;
 	
 	BEGIN
 		PROCESS(address) IS
